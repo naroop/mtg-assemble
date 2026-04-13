d7b50f598fc0aa57ed971208eea18176d04428e3
Nathaniel Rupp <nrupp815@outlook.com>
2026-04-13 08:59:31 -0500

    fix dual faced card import

--- mtg-assemble/src/util/deck-import.ts
+++ mtg-assemble/src/util/deck-import.ts
@@ -15,10 +15,10 @@ async function importDeck(deckId: string, text: string) {
       };
     });
 
-  const cards = await Cards.collection(...parsedDeck.map((card) => ({ name: card.name })));
+  const fetchedCards = await Cards.collection(...parsedDeck.map((card) => ({ name: card.name })));
 
   const cardsPayload = parsedDeck.map((card) => {
-    const scryfallCard = cards.find((c) => c.name === card.name);
+    const scryfallCard = fetchedCards.find((fetchedCard) => fetchedCard.name.startsWith(card.name));
 
     const oracleId = scryfallCard && scryfallCard.oracle_id ? scryfallCard.oracle_id : 'N/A';
 
